bc73b5e3083fa5c0132c5ecea67005f39ac95900
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-02-15 23:55:25 +0900

    일부 코드 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,26 @@
-alert("HI!")
+
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+const messagelist = document.querySelector("ul");
+const messageform = document.querySelector("form");
+
+socket.addEventListener("open", ()=>{
+    console.log("Connected to Browser!")
+})
+
+socket.addEventListener("message", (message)=>{
+    console.log(`Just got this : ${message.data} from the Server`)
+})
+
+socket.addEventListener("close", ()=>{
+    console.log(`disconnect from Server`)
+})
+
+function handleSubmit(e){
+    e.preventDefault();
+    const input = messageform.querySelector("input");
+    socket.send(input.value.toString());
+    input.value="";
+}
+
+messageform.addEventListener("submit", handleSubmit)
--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 import express from "express";
+import http from "http"
+import WebSocket from "ws";
 
 const app = express()
 app.set("view engine","pug")
-
 app.set("views",__dirname+"/views")
-
 app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (_,res)=> res.render("home"));
+app.get("/*", (_,res)=> res. redirect("/"));
 
-app.get("/", (req,res)=> res.render("home"));
+
 const handleListen =() => console.log(`Listening on HTTP://localhost:3000`)
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app) // express는 SW를 지원하지 않기 때문에 node의 http로 서버를 열어줘야함
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+
+wss.on("connection",(socket)=>{
+        sockets.push(socket);
+        socket.send("Hello!");
+        console.log("Connected with Browser");
+        socket.on("close", ()=>console.log("disconnected from Browser"));
+        socket.on("message",(message)=>{sockets.forEach(aSocket => aSocket.send(message.toString()))});
+    })
+
+server.listen(3000, handleListen);
